ca5217e0e66a838c2fcfeeffccc63ee12f9a3dd3
Adrian Ionut Zaharia <addyionut@gmail.com>
2021-05-13 12:13:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function generateButtons() {
+        let inputNumber = document.getElementById('inp').value;
+        let buttons = document.getElementsByTagName("body")[0];
+        var button ="";
+        var randomButtonNr = Math.ceil(Math.random() * inputNumber);
+        for (let i = 1; i <= inputNumber; i++) {
+          button = document.createElement("button");
+          button.innerHTML = 'Button ' + i;
+          buttons.appendChild(button);
+          button.onclick = function() {
+              pressButton(i);
+            };
+        }
+        function pressButton(currentButton) {  
+          if (randomButtonNr == currentButton) {
+            document.getElementById("print").innerHTML = "Congratulations! You got it right";
+          }
+          else {
+            document.getElementById("print").innerHTML = "Wrong button";
+          }
+        }   
+      }
